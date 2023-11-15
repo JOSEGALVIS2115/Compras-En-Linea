e17333ecd327c7b9c445a690e54956075bad2506
Jose Rodrigo Galvis Galvis <126744923+JOSEGALVIS2115@users.noreply.github.com>
2023-11-15 10:16:57 -0500

    primera subida

---
+++ script.js
@@ -0,0 +1,7 @@
+// hanmburguer menu 
+
+hamburguer = document.querySelector('.hamburguer');
+hamburguer.onclick = function () {
+    navBar = document.querySelector('.nav-bar');
+    navBar.classList.toggle('active')
+}
